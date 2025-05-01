2b60d54acaee8bb22f7758c88bb10ae6eb8f6462
kazu <test@gmail.com>
2025-05-01 16:11:03 +0900

    追加ボタン

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
             onClick={addTodo}
             className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
-            追加
+            追加ボタン
           </button>
         </div>
 
